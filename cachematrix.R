939a919761ae4130478c81953ccad6eac76ceaf3
rcpillai <renjithcpillai@gmail.com>
2016-03-04 19:48:16 -0500

    Week3 Assignment submission.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix is a function that returns a list of functions
+## The following are the functions:
+## setCacheMatrix      set the value of a matrix
+## getCacheMatrix      get the value of a matrix
+## setInverseMatrix   	get the cached value 
+## getInverseMatrix   	get the cached value 
 
-## Write a short comment describing this function
+  makeCacheMatrix <- function(a = matrix()) {
+  ## Define a null variable
+    cacheinvmatrix <- NULL
+    ## set a matrix
+    setCacheMatrix <- function(b) {
+      ## assign to a variable
+      a <<- b
+      ## cachedinvmatrix nullified because matrix got a new value
+      cacheinvmatrix <<- NULL
+      }
+    ## function to get a matrix
+    getCacheMatrix <- function(){ 
+    a
+    }
+  ## function to set Inverse Matrix
+  setInverseMatrix <- function(inverse){
+      cacheinvmatrix <<- inverse
+    }
+  ## function to get Inverse Matrix
+  getInverseMatrix <- function(){
+      cacheinvmatrix
+    }
+  ## returns a list, each argument is a function
+  list(setCacheMatrix = setCacheMatrix,
+       getCacheMatrix = getCacheMatrix,
+       setInverseMatrix = setInverseMatrix,
+       getInverseMatrix = getInverseMatrix)
+  }
 
-makeCacheMatrix <- function(x = matrix()) {
+  ## This function computes the inverse of the special "matrix" 
+  ## returned by makeCacheMatrix above
 
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  cacheSolve <- function(a, ...) {
+  ## get the cached inverse matrix
+    cacheinvmatrix <- a$getInverseMatrix()
+    ## check matrix
+    if (!is.null(cacheinvmatrix)) {
+      message("getting cached data")
+      return(cacheinvmatrix)
+    }
+  ## get data
+  matrixData <- a$getCacheMatrix()
+  ## calculate 
+  cacheinvmatrix <- solve(matrixData, ...)
+  ## set the matrix 
+  a$setInverseMatrix(cacheinvmatrix)
+  ## display 
+  cacheinvmatrix
 }
